aa81b3869bf8d3b9ee04ac5e5f9e6294a9ccddd3
tungdo <dosontung1010@gmail.com>
2021-06-23 10:15:51 +0700

    update for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_14_061104) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pokemon", force: :cascade do |t|
     t.string "name"
     t.string "type"
